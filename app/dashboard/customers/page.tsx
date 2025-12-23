b00934cb31758a40d0c576eced3f91857b8ff6dd
williamcs <solobird00@hotmail.com>
2025-12-23 16:49:50 +0800

    implement customer page search feature

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,25 +1,34 @@
-import { fetchFilteredCustomers } from '@/app/lib/data';
 import CustomersTable from '@/app/ui/customers/table';
+import { lusitana } from '@/app/ui/fonts';
+import Search from '@/app/ui/search';
+import { CustomersTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
     title: 'Customers',
 };
 
-export default async function Page({
-    searchParams,
-}: {
-    searchParams?: {
+export default async function Page(props: {
+    searchParams?: Promise<{
         query?: string;
         page?: string;
-    };
+    }>;
 }) {
+    const searchParams = await props.searchParams;
     const query = searchParams?.query || '';
-    const customers = await fetchFilteredCustomers(query);
 
     return (
-        <main>
-            <CustomersTable customers={customers} />
-        </main>
+        <div className="w-full">
+            <div className="flex w-full items-center justify-between">
+                <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+            </div>
+            <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+                <Search placeholder="Search customers..." />
+            </div>
+            <Suspense key={query} fallback={<CustomersTableSkeleton />}>
+                <CustomersTable query={query} />
+            </Suspense>
+        </div>
     );
 }
